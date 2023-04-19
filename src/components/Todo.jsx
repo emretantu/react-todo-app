5323239a183e5111d9a3a69265fb3c8ee54198bf
emretantu <emretantu@gmail.com>
2023-04-19 16:24:49 +0300

    item placeholder implemented

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,52 @@
+import { useRef } from "react";
 import classes from "./Todo.module.css"
 
 const Todo = ({ list, onChangingDone }) => {
 
   return (
     <ul className={classes.todo}>
-      {list.map(item => <ListItem key={item.id} itemData={item} onChangingDone={onChangingDone} />)}
+      {list.map((item, index) => <ListItem key={index} itemData={item} onChangingDone={onChangingDone} order={index} />)}
+      <Placeholder order={list.length} />
     </ul>
   );
 
 }
 
-const ListItem = ({ itemData, onChangingDone }) => {
+const ListItem = ({ itemData, onChangingDone, order }) => {
 
   return (
-    <li
-      className={ `${classes["todo__list-item"]} ${itemData.isDone ? classes["todo__list-item--done"] : ""}` }
-      draggable={true}
-      onClick={() => {onChangingDone(itemData)}}
-    >
-      {itemData.title}
+    <>
+      <Placeholder order={order} />
+      <li
+        className={ `${classes["todo__list-item"]} ${itemData.isDone ? classes["todo__list-item--done"] : ""}` }
+        draggable={true}
+        onClick={() => {onChangingDone(itemData)}}
+      >
+        {itemData.title}
+      </li>
+    </>
+  );
+
+}
+
+const Placeholder = ({ order }) => {
+
+  const phRef = useRef();
+  const addBtnRef = useRef();
+
+  const handleMouseEnter = () => {
+    phRef.current.classList.add(classes["placeholder--show"]);
+    addBtnRef.current.classList.add(classes["placeholder__button--show"]);
+  }
+
+  const handleMouseLeave = () => {
+    phRef.current.classList.remove(classes["placeholder--show"]);
+    addBtnRef.current.classList.remove(classes["placeholder__button--show"]);
+  }
+
+  return (
+    <li ref={phRef} className={ `${classes["placeholder"]}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <button ref={addBtnRef} className={ `${classes["placeholder__button"]}`}>+</button>
     </li>
   );
 
---
+++ src/components/TodoAddItem.jsx
@@ -0,0 +1,9 @@
+const TodoAddItem = () => {
+  return (
+    <form>
+      <input type="text" />
+    </form>
+  );
+}
+
+export default TodoAddItem;
